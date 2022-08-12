a88ccb6275cfecd1c2ccd4b8c7c1dcd36f28a7b6
Kota <maedakoutaa@gmail.com>
2022-08-12 21:32:42 +0900

    Initial Commit

---
+++ MiniApp77-SwiftUI/MiniApp77_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp77_SwiftUIApp.swift
+//  MiniApp77-SwiftUI
+//
+//  Created by 前田航汰 on 2022/08/12.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp77_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
