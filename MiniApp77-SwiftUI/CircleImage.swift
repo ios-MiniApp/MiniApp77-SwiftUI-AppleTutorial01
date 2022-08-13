ccc8dc3a61cc5b37aba84589047589ade8921299
Kota <maedakoutaa@gmail.com>
2022-08-13 11:38:44 +0900

    commit

---
+++ MiniApp77-SwiftUI/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  MiniApp77-SwiftUI
+//
+//  Created by 前田航汰 on 2022/08/13.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
