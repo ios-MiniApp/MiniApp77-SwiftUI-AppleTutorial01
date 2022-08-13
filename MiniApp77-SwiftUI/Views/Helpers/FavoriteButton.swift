997427e369286a25a8291d3f0b085301a0001cb6
Kota <maedakoutaa@gmail.com>
2022-08-13 16:30:01 +0900

    thirdCommit

---
+++ MiniApp77-SwiftUI/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  MiniApp77-SwiftUI
+//
+//  Created by 前田航汰 on 2022/08/13.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
