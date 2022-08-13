1dd27232a24401fbf255bb01eec7bec68f16af66
Kota <maedakoutaa@gmail.com>
2022-08-13 15:59:56 +0900

    secondcommi-リファクタリング

---
+++ MiniApp77-SwiftUI/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MiniApp77-SwiftUI
+//
+//  Created by 前田航汰 on 2022/08/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
